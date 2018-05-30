e664ebab4a3f61d3b4f7895e121d33236400e864
Sheean Spoel <s.j.j.spoel@uu.nl>
2018-05-30 17:32:16 +0200

    Return annotated xpath

---
+++ src/xpath.annotate.spec.ts
@@ -0,0 +1,48 @@
+import { XPathParser } from './xpath';
+
+describe('XPath Annotate', () => {
+    let parser: XPathParser;
+
+    beforeEach(() => {
+        parser = new XPathParser();
+    });
+
+    function runCommon(testCases: string[]) {
+        for (let testCase of testCases) {
+            try {
+                let annotated = parser.annotate(testCase);
+                expect(annotated.map(a => a.text).join('')).toEqual(testCase, "" + testCase + " parsed correctly.");
+            } catch (err) {
+                fail("" + err + " for input: " + testCase);
+            }
+        }
+    };
+
+    it("preserves whitespace", function () {
+        runCommon(["(5)",
+            "(( (( (5 )) )))  ",
+            "1 or 2 or 3",
+            "1 and 2 and 3",
+            "1 = 2 != 3 != 4 = 5",
+            "1 < 2 >= 3 <= 4 > 5",
+            "1 + 2 - 3 - 4 + 5",
+            "1 mod 2 div 3 div 4 * 5",
+            "1|2|3",
+            "/patient/sex = 'male' and /patient/age > 15",
+            "../jr:hist-data/labs[@type=\"cd4\"]",
+            "function_call(26*(7+3), //*, /im/child::an/ancestor::x[3][true()]/path)",
+            "function()",
+            "func:tion()",
+            "function(   )",
+            "function (5)",
+            "function   ( 5, 'arg', 4 * 12)",
+            "4andfunc()",
+            "1 < 2 = 3 > 4 and 5 <= 6 != 7 >= 8 or 9 and 10",
+            "1 * 2 + 3 div 4 < 5 mod 6 | 7 - 8",
+            "- 4 * 6",
+            "6*(3+4)and(5or2)",
+            "(1 - 2) - 3",
+            "1 - (2 - 3)"
+        ]);
+    });
+});
--- src/xpath.ts
+++ src/xpath.ts
@@ -16,12 +16,55 @@ export class XPathParser {
     }
 
     /**
-     * Parse an XPath string and returns a parse tree or throw a parse exception.
+     * Parses an XPath string and returns a parse tree or throw a parse exception.
      * @param input XPath string to parse
      * @exception @see {XPathModels.ParseError}
      */
     public parse(input: string): XPathModels.XPathExpression {
         XPathModels.CurrentHashtagConfig = this.hashtagConfig;
         return jison.parse(input);
+    }
+
+    /**
+     * Parses an XPath string and returns an annotated version of the input string.
+     * @param input XPath string to parse
+     * @exception @see {XPathModels.ParseError}
+     */
+    public annotate(input: string): XPathModels.XPathToken[] {
+        let parsed = this.parse(input);
+        let tokens = parsed.toTokens(false).filter(t => t.type != 'whitespace');
+        let index = 0;
+
+        // preserve whitespace
+        for (let i = 0; i < tokens.length; i++) {
+            let token = tokens[i];
+            // token starts here
+            let textOffset = input.substring(index).indexOf(token.text);
+            if (textOffset == -1) {
+                throw `Misaligned input and parse at ${index}. Could not find ${token.text} in input.`;
+            }
+            if (textOffset > 0) {
+                // inject token containing whitespace
+                tokens.splice(i, 0, {
+                    expression: parsed,
+                    text: input.substr(index, textOffset),
+                    type: 'whitespace'
+                });
+                index += textOffset;
+                i++;
+            }
+
+            index += token.text.length;
+        }
+
+        if (index < input.length) {
+            tokens.push({
+                expression: parsed,
+                text: input.substring(index),
+                type: 'whitespace'
+            });
+        }
+
+        return tokens;
     }
 }
